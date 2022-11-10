f74ebf9e2a27f46059623167007c05975f4afc33
elvvira <elvvira.castellanos@gmail.com>
2022-11-10 14:55:14 +0100

    formulario

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,12 +5,13 @@ const dateElement = document.getElementById('date');
 const cardNumberElement = document.getElementById('card-number');
 
 formElement.addEventListener('keyup', (event)=>{
-    console.log(formElement.cvc.value);
+ 
     cvcElement.textContent = (formElement.cvc.value);
-    console.log(formElement.name.value);
+
     nameElement.textContent = (formElement.name.value);
-    console.log(formElement.date.value);
+    
     dateElement.textContent = (formElement.date.value);
-    console.log(formElement.card.value);
+    
     cardNumberElement.textContent = (formElement.card.value);
+    
 })
